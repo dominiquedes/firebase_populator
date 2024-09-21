573fb9d3206355b69ec5af019e922f6c28fd9853
Dominique Desert <160805897+dominiquedes@users.noreply.github.com>
2024-09-21 13:28:00 -0400

    Delete build/lib/firebase_populator directory

--- build/lib/firebase_populator/__init__.py
+++
@@ -1,9 +0,0 @@
-# firebase_populator/__init__.py
-
-"""
-firebase_populator: A package to populate Firebase Firestore with generated data.
-"""
-
-__version__ = '0.1.0'  # Package version
-
-from .firebase_populator import FirebasePopulator
