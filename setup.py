c4238bbb4b3648ef70a2cf208d87adf5da2bad7e
dominiquedes <dominiquedesert@Dominiques-MacBook-Pro.local>
2024-09-21 13:05:52 -0400

    adjust setup

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='firebase-populator',
     version='0.1.0',
-    description='A package to populate Firebase Firestore.',
     author='Your Name',
     author_email='your.email@example.com',
     packages=find_packages(),
     install_requires=[
         'firebase-admin',  # This will ensure firebase-admin is installed
     ],
+    description="A brief description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
